dbfad91d2f3f6dc521bb2adb84942c814824e68e
Artem <hitrick84@ya.ru>
2015-09-30 18:38:03 +0400

    add routes

--- route.js
+++ route.js
@@ -1,11 +1,11 @@
 module.exports = function(router) {
 	router.get("/", function(req, res) {
-		res.end("Main page");
+		res.render("frontend/index", {title: "Main page"});
 	});
 	router.get("/cpu", function(req, res) {
-		res.end("Test CPU Page");
+		res.end("/cpu", "Test CPU Page");
 	});
 	router.get("/vcard", function(req, res) {
-		res.end("Test Video cards Page");
+		res.end("Test Vard Page");
 	});
 };
--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var router = express.Router();
-require("./route")(router);
 var app = express();
 
 app.use(bodyParser({extended: "true"}));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
-
+app.set("views", __dirname + "/views");
+require("./route")(router);
 var port = process.env.port || 8686;
-
-
 
 app.use("/", router);
 app.listen(port);
